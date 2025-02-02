5fdafeeebff56c9f5de30c9f28e7f02e46042596
gabrielam0ura <gabrielamoura@alu.ufc.br>
2025-02-02 16:56:08 -0300

    First commit

---
+++ back/src/test/java/todo/todo/TodoApplicationTests.java
@@ -0,0 +1,14 @@
+package todo.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.todo.TodoApplication;
+
+@SpringBootTest(classes = TodoApplication.class)
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
